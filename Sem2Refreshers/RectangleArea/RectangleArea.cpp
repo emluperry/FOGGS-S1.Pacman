1fd6a957d0b449816bbdb00181095c0743559810
Emily Perry <emluperry@gmail.com>
2022-02-01 13:19:40 +0000

    RectangleArea Plus Header File

--- Sem2Refreshers/RectangleArea/RectangleArea.cpp
+++ Sem2Refreshers/RectangleArea/RectangleArea.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-using namespace std;
-
-void InputRectangle(int& width, int& height);
-void OutputRectangleArea(const int width, const int height);
-void OutputRectanglePerimeter(const int width, const int height);
+#include "Header.h"
 
 int main()
 {
