91ef5c990601eaa284948ae908a6ab8f2bb5cbfb
Emily Perry <emluperry@gmail.com>
2022-02-01 13:18:54 +0000

    RectangleArea

---
+++ Sem2Refreshers/RectangleArea/RectangleArea.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void InputRectangle(int& width, int& height);
+void OutputRectangleArea(const int width, const int height);
+void OutputRectanglePerimeter(const int width, const int height);
+
+int main()
+{
+    int w = 0;
+    int h = 0;
+    InputRectangle(w, h);
+
+    OutputRectangleArea(w, h);
+    OutputRectanglePerimeter(w, h);
+    return 0;
+}
+
+void InputRectangle(int& width, int& height)
+{
+    cout << "Please input the width of the rectangle in cm." << endl;
+    cin >> width;
+    cout << "Please input the height of the rectangle in cm." << endl;
+    cin >> height;
+}
+
+void OutputRectangleArea(const int width, const int height)
+{
+    cout << "The area is " << width * height << " centimetres squared." << endl;
+}
+
+void OutputRectanglePerimeter(const int width, const int height)
+{
+    cout << "The perimeter is " << width * 2 + height * 2 << " centimetres." << endl;
+}
