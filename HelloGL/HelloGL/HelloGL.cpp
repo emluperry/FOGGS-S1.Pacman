eb049cb17a96e9aa5c197b107966dc8e55dc85e4
Emily Perry <emluperry@gmail.com>
2022-02-01 14:18:25 +0000

    Creating a Rectangle

--- HelloGL/HelloGL/HelloGL.cpp
+++ HelloGL/HelloGL/HelloGL.cpp
@@ -12,7 +12,16 @@ HelloGL::HelloGL(int argc, char* argv[])
 
 void HelloGL::Display()
 {
-
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+	glVertex2f(-0.75, 0.5);
+	glVertex2f(0.75, 0.5);
+	glColor4f(0.0f, 0.0f, 0.0f, 0.0f);
+	glVertex2f(0.75, -0.5);
+	glVertex2f(-0.75, -0.5);
+	glEnd();
+	glFlush();
 }
 
 HelloGL::~HelloGL(void)
