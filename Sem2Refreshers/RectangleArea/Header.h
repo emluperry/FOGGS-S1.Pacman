3102764af8c3ab9a9708d02df0f139c5fa45b355
Osiristic <emluperry@gmail.com>
2022-02-03 11:55:06 +0000

    Part 1 of Semester 2 Work migrated

--- Sem2Refreshers/RectangleArea/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-
-void InputRectangle(int& width, int& height);
-void OutputRectangleArea(const int width, const int height);
-void OutputRectanglePerimeter(const int width, const int height);
